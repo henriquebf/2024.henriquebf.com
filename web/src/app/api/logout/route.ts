5aca281408bb1cc01d735a1cfa0e0f3df674bab9
Henrique Ferreira <henriquebf@gmail.com>
2024-04-17 22:29:12 +0200

    implement strava api using route handlers

---
+++ web/src/app/api/logout/route.ts
@@ -0,0 +1,10 @@
+import { type NextRequest } from "next/server";
+import { redirect } from "next/navigation";
+import { getSession } from "@/lib/session";
+
+export async function GET() {
+  const session = await getSession();
+  // Destroy session and return to Index page
+  session.destroy();
+  redirect("/admin");
+}
--- web/src/lib/session.ts
+++ web/src/lib/session.ts
@@ -1,42 +1,24 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 // see: https://github.com/vvo/iron-session
-import {
-  GetServerSidePropsContext,
-  GetServerSidePropsResult,
-  NextApiHandler,
-} from 'next';
-import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next';
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
 
 const sessionOptions = {
-  password: process.env.SESSION_SECRET || '',
-  cookieName: 'gearalert',
+  password: process.env.SESSION_SECRET || "",
+  cookieName: "gearalert",
   cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
   },
 };
 
 if (!process.env.SESSION_SECRET) {
-  throw new Error('session: SESSION_SECRET not provided!');
+  throw new Error("session: SESSION_SECRET not provided!");
 }
 
-export function withSessionRoute(handler: NextApiHandler) {
-  return withIronSessionApiRoute(handler, sessionOptions);
+export interface Session {
+  athleteId?: string;
 }
 
-// Theses types are compatible with InferGetStaticPropsType https://nextjs.org/docs/basic-features/data-fetching#typescript-use-getstaticprops
-export function withSessionSsr<
-  P extends { [key: string]: unknown } = { [key: string]: unknown }
->(
-  handler: (
-    context: GetServerSidePropsContext
-  ) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>
-) {
-  return withIronSessionSsr(handler, sessionOptions);
-}
-
-// This is where we specify the typings of req.session.*
-declare module 'iron-session' {
-  interface IronSessionData {
-    athleteId?: string;
-  }
+export function getSession() {
+  return getIronSession(cookies(), sessionOptions);
 }
