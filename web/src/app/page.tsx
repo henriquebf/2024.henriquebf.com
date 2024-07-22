3f6a26db632ad085e73c5a9400165b34e8e7fc73
Henrique Ferreira <henriquebf@gmail.com>
2024-07-22 13:41:41 +0200

    move cache settings to page component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,6 +5,8 @@ import ExperienceSection from "./(experience)/experience";
 import ProjectsSection from "./(projects)/projects";
 import Header from "./(header)/header";
 
+export const revalidate = 1000 * 60 * 60 * 12; // Revalidate this route caching in 12 hours
+
 export default function Home() {
   return (
     <>
