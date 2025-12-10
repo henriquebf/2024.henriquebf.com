d116ac7c996b9e958923537a6bd22f64c5c0f807
Henrique Ferreira <henriquebf@gmail.com>
2025-12-10 11:27:39 +0100

    log webhook testing in console

--- web/src/app/api/webhooks/test/route.ts
+++ web/src/app/api/webhooks/test/route.ts
@@ -1,7 +1,9 @@
 export async function GET() {
+  console.log("Testing GET Webhook...");
   return Response.json({ result: "GET:OK" }, { status: 200 });
 }
 
 export async function POST() {
+  console.log("Testing POST Webhook...");
   return Response.json({ result: "POST:OK" }, { status: 200 });
 }
