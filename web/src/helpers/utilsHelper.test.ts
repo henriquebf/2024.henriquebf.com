38bbef8a8881d371cb6eb7834485dd3058e03060
Henrique Ferreira <henriquebf@gmail.com>
2026-09-13 19:59:07 +0200

    fix dependency vulnerabilities

--- web/src/helpers/utilsHelper.test.ts
+++ web/src/helpers/utilsHelper.test.ts
@@ -2,14 +2,14 @@ import { classNames } from './utilsHelper';
 
 describe('classNames', () => {
   it('Single classname', () => {
-    expect(classNames(['xxx'])).toEqual('xxx');
+    expect(classNames(['xxx'])).to.equal('xxx');
   });
 
   it('Multiple classnames', () => {
-    expect(classNames(['xxx', 'yyy', 'zzz'])).toEqual('xxx yyy zzz');
+    expect(classNames(['xxx', 'yyy', 'zzz'])).to.equal('xxx yyy zzz');
   });
 
   it('Multiple classnames with falsy', () => {
-    expect(classNames(['xxx', 'yyy', 'zzz', undefined])).toEqual('xxx yyy zzz');
+    expect(classNames(['xxx', 'yyy', 'zzz', undefined])).to.equal('xxx yyy zzz');
   });
 });
